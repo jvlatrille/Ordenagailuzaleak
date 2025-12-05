c72095d45f5c258d6492fd06ca8979972e034dd3
mouhakeita23@gmail.com <mouhakeita23@gmail.com>
2025-12-05 03:56:41 +0100

    page prete sans vidéo

--- src/app/features/carte-talents/main-carte-talents/main-carte-talents.component.ts
+++ src/app/features/carte-talents/main-carte-talents/main-carte-talents.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+
 interface TeamMember {
   name: string;
   surname: string;
@@ -12,88 +13,8 @@ interface TeamMember {
   selector: 'app-main-carte-talents',
   imports: [CommonModule],
   templateUrl: './main-carte-talents.component.html',
-  styleUrl: './main-carte-talents.component.css'
+  styleUrls: ['./main-carte-talents.component.css']
 })
 export class MainCarteTalentsComponent {
-  // Membres du Bureau
-  bureauMembers: TeamMember[] = [
-    {
-      name: 'Prénom',
-      surname: 'NOM',
-      role: 'Président',
-      image: 'assets/team/placeholder.jpg'
-    },
-    {
-      name: 'Prénom',
-      surname: 'NOM',
-      role: 'Vice-Président',
-      image: 'assets/team/placeholder.jpg'
-    },
-    {
-      name: 'Prénom',
-      surname: 'NOM',
-      role: 'Secrétaire',
-      image: 'assets/team/placeholder.jpg'
-    },
-    {
-      name: 'Prénom',
-      surname: 'NOM',
-      role: 'Trésorier',
-      image: 'assets/team/placeholder.jpg'
-    }
-  ];
-
-  // Pôle Communication
-  communicationMembers: TeamMember[] = [
-    {
-      name: 'Prénom',
-      surname: 'NOM',
-      role: 'Responsable Com',
-      image: 'assets/team/placeholder.jpg'
-    },
-    {
-      name: 'Prénom',
-      surname: 'NOM',
-      role: 'Community Manager',
-      image: 'assets/team/placeholder.jpg'
-    }
-  ];
-
-  // Pôle Développement
-  devMembers: TeamMember[] = [
-    {
-      name: 'Prénom',
-      surname: 'NOM',
-      role: 'Lead Developer',
-      image: 'assets/team/placeholder.jpg'
-    },
-    {
-      name: 'Prénom',
-      surname: 'NOM',
-      role: 'Développeur Full-Stack',
-      image: 'assets/team/placeholder.jpg'
-    },
-    {
-      name: 'Prénom',
-      surname: 'NOM',
-      role: 'Développeur Front-End',
-      image: 'assets/team/placeholder.jpg'
-    }
-  ];
-
-  // Pôle Technique
-  techMembers: TeamMember[] = [
-    {
-      name: 'Prénom',
-      surname: 'NOM',
-      role: 'Responsable Technique',
-      image: 'assets/team/placeholder.jpg'
-    },
-    {
-      name: 'Prénom',
-      surname: 'NOM',
-      role: 'Admin Système',
-      image: 'assets/team/placeholder.jpg'
-    }
-  ];
+  
 }
