4add7ae043f347604b4c7b6e3e89c526dc2b7dd5
Iban ARANDIA <arandiaiban@gmail.com>
2025-12-05 04:08:49 +0100

    hopla

--- src/app/features/anti-gafam/main-anti-gafam/main-anti-gafam.component.ts
+++ src/app/features/anti-gafam/main-anti-gafam/main-anti-gafam.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
+import { SpaceInvader } from '../space-invader/space-invader';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main-anti-gafam',
-  imports: [],
+  imports: [SpaceInvader, CommonModule ],
   templateUrl: './main-anti-gafam.component.html',
   styleUrl: './main-anti-gafam.component.css'
 })
 export class MainAntiGafamComponent {
+  isLoadingGame: boolean = false; // Pour contrôler l'animation de chargement
+  isGameActive: boolean = false; // Pour activer le composant de jeu
 
+  constructor(private renderer: Renderer2) { }
+
+  startGame(): void {
+    this.isGameActive = !this.isGameActive; // Démarre l'animation de chargement
+
+  }
 }
